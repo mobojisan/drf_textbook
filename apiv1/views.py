6f8109a32a93188a053625d0ae086350308831f1
mobojisan <r.kasubuchi@xmart.co.jp>
2022-07-27 20:27:38 +0900

    ModelSerializerをGenericViewSetと各種Mixinからの継承で一部置き換え

--- apiv1/views.py
+++ apiv1/views.py
@@ -1,12 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from shop.models import Book
 from .serializers import BookSerializer
 
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.UpdateModelMixin):
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+    
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
